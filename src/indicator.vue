65e2924b42e24bbca98bc48600be2dc0081b1c5b
Leopoldthecoder <Leopoldthecuber@gmail.com>
2016-05-19 14:36:11 +0800

    remove api

--- src/indicator.vue
+++ src/indicator.vue
@@ -1,36 +1,37 @@
 <template>
-  <div class="indicator d-popup-center" v-if="rendered" v-show="visible" transition="pop-fade">
-    <span class="indicator-text" v-show="text">{{ text }}</span>
-    <span class="indicator-spin"></span>
+  <div class="kebab-indicator" v-if="rendered" v-show="visible" transition="pop-bounce" :style="{ 'padding': text ? '20px' : '10px' }">
+    <span class="kebab-indicator-spin"></span>
+    <span class="kebab-indicator-text" v-show="text">{{ text }}</span>
   </div>
 </template>
 
 <style>
-  .indicator {
-    position: fixed;
-    border-radius: 5px;
-    background: rgba(0, 0, 0, 0.7);
-    color: white;
-    box-sizing: border-box;
-    padding: 10px;
-    text-align: center;
-  }
+  @component-namespace kebab {
+    @component indicator {
+      position: fixed 50% * * 50%;
+      transform: translate(-50%, -50%);
+      border-radius: 5px;
+      background: rgba(0, 0, 0, 0.7);
+      color: white;
+      box-sizing: border-box;
+      text-align: center;
 
-  .indicator-text {
-    display: block;
-    color: #fff;
-    text-align: center;
-    margin-bottom: 10px;
-  }
+      @descendent text {
+        display: block;
+        color: #fff;
+        text-align: center;
+        margin-top: 10px;
+      }
 
-  .indicator-spin {
-    display: inline-block;
-    font-size: 0;
-    width: 40px;
-    height: 40px;
-    background: url("loading.svg") center center no-repeat;
-    background-size: 100%;
-    animation: indicator-spin 1s steps(12,end) infinite;
+      @descendent spin {
+        display: inline-block;
+        font-size: 0;
+        size: 40px;
+        background: url("loading.svg") center center no-repeat;
+        background-size: 100%;
+        animation: indicator-spin 1s steps(12,end) infinite;
+      }
+    }
   }
 
   @keyframes indicator-spin {
@@ -38,11 +39,26 @@
       transform: rotate(360deg);
     }
   }
+
+  .pop-bounce-transition {
+    transition: opacity .3s linear;
+  }
+
+  .pop-bounce-enter,
+  .pop-bounce-leave {
+    opacity: 0;
+  }
+
+  .d-modal {
+    position: fixed 0 * * 0;
+    size: 100%;
+    opacity: 0;
+    background: transparent;
+  }
 </style>
 
 <script type="text/babel">
   import Popup from 'el-popup';
-  require('el-popup/dist/style.css');
 
   export default {
     mixins: [ Popup ],
@@ -51,11 +67,11 @@
       text: String,
       modal: {
         type: Boolean,
-        default: false
+        default: true
       },
       closeOnClickModal: {
         type: Boolean,
-        default: true
+        default: false
       }
     }
   };
